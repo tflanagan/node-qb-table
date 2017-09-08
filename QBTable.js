4a6da574fc33ae8d7bccb1abf518333a99155b62
Tristian Flanagan <tflanagan@datacollaborative.com>
2017-09-08 10:14:37 -0400

    src: rework what _load returns

--- QBTable.js
+++ QBTable.js
@@ -3,7 +3,7 @@
 /* Versioning */
 const VERSION_MAJOR = 1;
 const VERSION_MINOR = 9;
-const VERSION_PATCH = 1;
+const VERSION_PATCH = 2;
 
 /* Dependencies */
 const merge = require('lodash.merge');
@@ -300,13 +300,13 @@ class QBTable {
 			options: localOptions || this.getOptions(),
 			includeRids: true
 		}).then((results) => {
-			return this._load(results, localClist, preserve).then((records) => {
-				if(returnRaw){
-					return results;
-				}
+			const records = this._load(results, localClist, preserve);
 
-				return records;
-			});
+			if(returnRaw){
+				return results;
+			}
+
+			return records;
 		});
 	};
 
