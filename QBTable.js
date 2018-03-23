ee8546b6edc48d18b43494e81d99e728c9346053
Tristian Flanagan <tflanagan@datacollaborative.com>
2018-03-24 01:57:53 +0200

    deps: updated qb-record

--- QBTable.js
+++ QBTable.js
@@ -3,7 +3,7 @@
 /* Versioning */
 const VERSION_MAJOR = 1;
 const VERSION_MINOR = 10;
-const VERSION_PATCH = 10;
+const VERSION_PATCH = 11;
 
 /* Dependencies */
 const merge = require('lodash.merge');
